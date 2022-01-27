2cb20e141d498013e87b2193d4d5f16f5144c42d
VMila <vi-milanez@hotmail.com>
2022-01-26 22:29:49 -0300

    contadorzin

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const visor = document.querySelector('.visor')
-
+var visor = document.querySelector('.visor')
 function somar() {
- visor.textContent++;
+    
+    visor.textContent = visor.textContent - -(document.getElementById('coiso').value);
+    console.log(document.getElementById('coiso').value); 
+    
 }
 
 function subtrair() {
-    visor.textContent--;
+    
+    visor.textContent = visor.textContent-  document.getElementById('coiso').value;
+    
    }
    
