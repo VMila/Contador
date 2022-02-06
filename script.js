815544f81de5b496795ae44021197b29c55d3669
VMila <vi-milanez@hotmail.com>
2022-02-05 22:23:00 -0300

    corzinha

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ function somar() {
     
     visor.textContent = visor.textContent - -(document.getElementById('coiso').value);
     console.log(document.getElementById('coiso').value); 
+    if(visor.textContent > 0)
+    visor.style.color = "green";
+    if(visor.textContent == 0)
+    visor.style.color = "black";
+    if(visor.textContent < 0)
+    visor.style.color = "red";
     
 }
 
 function subtrair() {
     
     visor.textContent = visor.textContent-  document.getElementById('coiso').value;
+    if(visor.textContent > 0)
+    visor.style.color = "green";
+    if(visor.textContent == 0)
+    visor.style.color = "black";
+    if(visor.textContent < 0)
+    visor.style.color = "red";
     
    }
 
    function reseta() {
-    visor.textContent = 0;  
+    visor.textContent = 0; 
+    visor.style.color = "black" 
    }
    
